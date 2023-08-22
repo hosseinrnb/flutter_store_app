62fa4ff47e7464831fd0749aa966640b67d86dd4
hossein <hosseinravanbakhsh.3238@gmail.com>
2023-08-22 15:00:24 +0330

    some changes

--- flutter_store_app/lib/di/di.dart
+++ flutter_store_app/lib/di/di.dart
@@ -20,40 +20,53 @@ import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 var locator = GetIt.instance;
-
-
 Future<void> getItInit() async {
+  await _initComponents();
 
-  //util
-  locator.registerSingleton<PaymentHandler>(ZarinpalPaymentHandler(locator.get()));
+  _initDatasoruces();
+
+  _initRepositories();
+
+  locator
+      .registerSingleton<BasketBloc>(BasketBloc(locator.get(), locator.get()));
+}
+
+Future<void> _initComponents() async {
   locator.registerSingleton<UrlHandler>(UrlLauncher());
-  
-  //components
-  locator.registerSingleton<Dio>(Dio(BaseOptions(baseUrl: 'http://startflutter.ir/api/')));
-  locator.registerSingleton<SharedPreferences>(await SharedPreferences.getInstance());
+  locator
+      .registerSingleton<PaymentHandler>(ZarinpalPaymentHandler(locator.get()));
+  locator.registerSingleton<Dio>(
+      Dio(BaseOptions(baseUrl: 'http://startflutter.ir/api/')));
+  locator.registerSingleton<SharedPreferences>(
+      await SharedPreferences.getInstance());
+}
 
-
-  //datasources
-  locator.registerFactory<IAuthenticationDatasource>(() => AuthenticationRemote());
-  locator.registerFactory<ICategoryDatasource>(() => CategoryRemoteDatasource());
+void _initDatasoruces() {
+  locator
+      .registerFactory<IAuthenticationDatasource>(() => AuthenticationRemote());
+  locator
+      .registerFactory<ICategoryDatasource>(() => CategoryRemoteDatasource());
   locator.registerFactory<IBannerDatasource>(() => BannerRemoteDatasource());
   locator.registerFactory<IProductDatasource>(() => ProductRemoteDatasource());
-  locator.registerFactory<IDetailProductDatasource>(() => DetailProductRemoteDatasource());
-  locator.registerFactory<ICategoryProductDatasource>(() => CategoryProductRemoteDatasource());
+  locator.registerFactory<IDetailProductDatasource>(
+      () => DetailProductRemoteDatasource());
+  locator.registerFactory<ICategoryProductDatasource>(
+      () => CategoryProductRemoteDatasource());
   locator.registerFactory<IBasketDatasource>(() => BasketLocalDatasource());
 
+  //locator.registerFactory<ICommentDatasource>(() => CommentRemoteDatasource());
+}
 
-  //repositories
+void _initRepositories() {
   locator.registerFactory<IAuthRepository>(() => AuthenticationRepository());
   locator.registerFactory<ICategoryRepository>(() => CategoryRepository());
   locator.registerFactory<IBannerRepository>(() => BannerRepository());
   locator.registerFactory<IProductRepository>(() => ProductRepository());
-  locator.registerFactory<IDetailProductRepository>(() => DetailProductRepository());
-  locator.registerFactory<ICategoryProductRepository>(() => CategoryProductRepository());
+  locator.registerFactory<IDetailProductRepository>(
+      () => DetailProductRepository());
+  locator.registerFactory<ICategoryProductRepository>(
+      () => CategoryProductRepository());
   locator.registerFactory<IBasketRepository>(() => BasketRepository());
 
-
-  //bloc
-  locator.registerSingleton<BasketBloc>(BasketBloc(locator.get(), locator.get()));
-
+  //locator.registerFactory<ICommentRepository>(() => CommentRepository());
 }
